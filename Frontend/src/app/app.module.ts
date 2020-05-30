17d792c3cfe460bfbdf1014b74ae34c62f7e1ddd
misakyury <yurymisak@coherentsolutions.com>
2020-05-30 20:27:32 +0300

    [Frontend]: functioning login

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+		HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+		AppRoutingModule,
+		FormsModule,
+		LoginModule,
+		ReactiveFormsModule,
+		HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
